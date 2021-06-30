142a9d4fc87f4ba38b5b42c4c72e8bec14cd424a
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-06-30 21:45:14 +0200

    Task 3: add role class with complete annotations

---
+++ backend/examportalserver/src/main/java/com/examportal/server/models/Role.java
@@ -0,0 +1,22 @@
+package com.examportal.server.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "roles",schema = "public")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
