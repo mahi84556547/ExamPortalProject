fa4d480879b4dc68c7c7a54c95a8fa6b80914136
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-06-30 23:30:48 +0200

    Task 7: add user service interface

---
+++ backend/examportalserver/src/main/java/com/examportal/server/services/UserService.java
@@ -0,0 +1,11 @@
+package com.examportal.server.services;
+
+import com.examportal.server.models.User;
+import com.examportal.server.models.UserRole;
+
+import java.util.Set;
+
+public interface UserService {
+
+    public User saveUser(User user, Set<UserRole> userRoleSet);
+}
