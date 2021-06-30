c1a1f25eb8db305edfbf7274633fec080bc5459e
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-06-30 20:31:57 +0200

    Task 0: first commit & upload the project

---
+++ backend/examportalserver/src/main/java/com/examportalserver/ExamportalserverApplication.java
@@ -0,0 +1,13 @@
+package com.examportalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamportalserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamportalserverApplication.class, args);
+    }
+
+}
---
+++ backend/examportalserver/src/test/java/com/examportalserver/ExamportalserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examportalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamportalserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
