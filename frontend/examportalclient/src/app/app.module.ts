454d188b3bbc84a317f467a7324bcabf006ad1be
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-01 12:51:28 +0200

    Task 17: add footer component

--- frontend/examportalclient/src/app/app.module.ts
+++ frontend/examportalclient/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
