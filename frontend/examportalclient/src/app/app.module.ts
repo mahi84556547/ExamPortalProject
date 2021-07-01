02ef9dd6c01ecefb3ad47d7df9821a781fbbbbbe
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-01 14:43:59 +0200

    Task 21: update angular.json (update theme name) & signup.component

--- frontend/examportalclient/src/app/app.module.ts
+++ frontend/examportalclient/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -23,6 +26,8 @@ import { SigninComponent } from './pages/signin/signin.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
