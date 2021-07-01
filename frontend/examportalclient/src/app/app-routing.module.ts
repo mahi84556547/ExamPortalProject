c2fdf1ca75f13afb53c483fe03713c69d33cf085
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-01 13:02:00 +0200

    Task 20: update app.routing & app.component (handle signup path)

--- frontend/examportalclient/src/app/app-routing.module.ts
+++ frontend/examportalclient/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { SignupComponent } from './pages/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"sginup",component:SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
