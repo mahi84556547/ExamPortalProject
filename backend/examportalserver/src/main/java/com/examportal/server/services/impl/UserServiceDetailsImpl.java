65aff0cf2f44571191ac457f37add936264e295f
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-02 11:24:13 +0200

    Task 32: add user_service_details_impl class

---
+++ backend/examportalserver/src/main/java/com/examportal/server/services/impl/UserServiceDetailsImpl.java
@@ -0,0 +1,30 @@
+package com.examportal.server.services.impl;
+
+import com.examportal.server.models.User;
+import com.examportal.server.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserServiceDetailsImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+    @Autowired
+    public UserServiceDetailsImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByUsername(username);
+        if(user==null){
+            throw new UsernameNotFoundException("username not found with given user name:" + username);
+        }
+        return user;
+    }
+}
