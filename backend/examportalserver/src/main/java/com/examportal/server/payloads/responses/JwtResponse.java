c8a9d216f95a349e132f6fe140e1cbf078eb5da5
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-02 09:51:05 +0200

    Task 31: add jwt request & response class

---
+++ backend/examportalserver/src/main/java/com/examportal/server/payloads/responses/JwtResponse.java
@@ -0,0 +1,14 @@
+package com.examportal.server.payloads.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class JwtResponse {
+    public String token;
+}
