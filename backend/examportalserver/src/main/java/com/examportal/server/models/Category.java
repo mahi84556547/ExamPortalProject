fd550bcfbf5eaf8219f17547248b4385d13d1b25
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-07 09:39:01 +0200

    Task 43: add category class (backend)

---
+++ backend/examportalserver/src/main/java/com/examportal/server/models/Category.java
@@ -0,0 +1,27 @@
+package com.examportal.server.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "categories",schema = "public")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+}
