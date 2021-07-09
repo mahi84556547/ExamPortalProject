20f23711cb0b4c6e7aa2572b23e79fb3f2a5350b
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-09 22:32:12 +0200

    Task 54: add question service impl (backend)

--- backend/examportalserver/src/main/java/com/examportal/server/services/QuestionService.java
+++ backend/examportalserver/src/main/java/com/examportal/server/services/QuestionService.java
@@ -1,4 +1,13 @@
 package com.examportal.server.services;
 
+import com.examportal.server.models.Question;
+
+import java.util.Set;
+
 public interface QuestionService {
+    public Set<Question> getQuestions();
+    public Question getQuestion(Long questionId);
+    public Question createQuestion(Question question);
+    public Question updateQuestion(Question question);
+    public void deleteQuestion(Long questionId);
 }
---
+++ backend/examportalserver/src/main/java/com/examportal/server/services/impl/QuestionServiceImpl.java
@@ -0,0 +1,51 @@
+package com.examportal.server.services.impl;
+
+import com.examportal.server.models.Question;
+import com.examportal.server.repositories.QuestionRepository;
+import com.examportal.server.services.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Transactional
+public class QuestionServiceImpl implements QuestionService {
+
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionServiceImpl(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    @Override
+    public Set<Question> getQuestions() {
+        return new HashSet<>(questionRepository.findAll());
+    }
+
+    @Override
+    public Question getQuestion(Long questionId) {
+        return questionRepository.findById(questionId).get();
+    }
+
+    @Override
+    public Question createQuestion(Question question) {
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public Question updateQuestion(Question question) {
+        return questionRepository.save(question);
+    }
+
+    @Override
+    public void deleteQuestion(Long questionId) {
+        Question question=getQuestion(questionId);
+        if(question != null){
+            questionRepository.deleteById(questionId);
+        }
+    }
+}
