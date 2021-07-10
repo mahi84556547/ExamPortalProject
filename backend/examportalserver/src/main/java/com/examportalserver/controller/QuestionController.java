741713b88b01c4aae59b76dbd0689b43796d92d6
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-07-10 17:49:39 +0200

    Task 60: update question controller getQuestionsByQuizzId (backend)

--- backend/examportalserver/src/main/java/com/examportalserver/controller/QuestionController.java
+++ backend/examportalserver/src/main/java/com/examportalserver/controller/QuestionController.java
@@ -1,23 +1,30 @@
 package com.examportalserver.controller;
 
 import com.examportalserver.models.Question;
+import com.examportalserver.models.Quizz;
 import com.examportalserver.services.QuestionService;
+import com.examportalserver.services.QuizzService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("questions")
 public class QuestionController {
+
     private QuestionService questionService;
+    private QuizzService quizzService;
     
     @Autowired
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, QuizzService quizzService) {
         this.questionService = questionService;
+        this.quizzService=quizzService;
     }
 
     @GetMapping
@@ -34,8 +41,15 @@ public class QuestionController {
 
     @GetMapping("quizz/{quizzId}")
     public ResponseEntity<List<Question>> getQuestionByQuizzId(@PathVariable(name = "quizzId") Long quizzId){
-        List<Question>  questions=questionService.getQuestionByQuizz(quizzId);
-        return new ResponseEntity<List<Question> >(questions, HttpStatus.OK);
+        Quizz quizz=this.quizzService.getQuizz(quizzId);
+        Set<Question> questions=quizz.getQuestionSet();
+        List list=new ArrayList(questions);
+        if(list.size()>Integer.parseInt(quizz.getNumberOfQuestions())){
+            list=list.subList(0,Integer.parseInt(quizz.getNumberOfQuestions() + 1));
+        }
+        Collections.shuffle(list);
+        //List<Question>  questions=questionService.getQuestionByQuizz(quizzId);
+        return new ResponseEntity<List<Question> >(list, HttpStatus.OK);
     }
 
     @PostMapping
